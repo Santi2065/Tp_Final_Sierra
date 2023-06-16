788e1991051fe5785e99b08b28c8073fd8115f12
Santiago Groba <santiagogroba@hotmail.com>
2023-06-15 22:04:53 -0300

    correcciones hikers(hablar con Groba)

--- HIKERS.py
+++ HIKERS.py
@@ -1,17 +1,18 @@
 from communication.client.client import MountainClient
 import random
 import math
-import time
 
-c = MountainClient('10.42.0.1',8888)
+c = MountainClient()
 
 class Hiker:
-    def __init__(self,ordenes: list,nombre:str):
+    
+    def __init__(self, nombre:str, ordenes: list = {'direction':0,'speed':50}):
 
         self.nombre = nombre # Hacer esto automaticamente (que no haya que pasarlo como arg de clase)
-        self.ordenes = ordenes # Es una lista así lo puedo modiciar en el marco global. Lo uso como diccionario.
+        self.ordenes = ordenes # Es una lista así lo puedo modificar en el marco global. Lo uso como diccionario.
         self.radio_montania = 23000
         self.team = 'Los cracks' # Hacer esto automaticamente
+    
     def almost_out(self)-> bool:
         """Devuelve verdadero si el escalador se ira del mapa en la siguiente iteracion. Falso en caso contrario."""
         dic = c.get_data()
@@ -83,36 +84,28 @@ class Hiker:
         return (x,y,z)
 
 
-
-c.add_team('Los cracks', ['Gian','Gian2','Gian3','Gian4'])
-c.finish_registration()
-
-directives = {'Gian':{'direction':0,'speed':50}, 'Gian2':{'direction':10,'speed':50},'Gian3':{'direction':5,'speed':50},'Gian4':{'direction':3,'speed':50}}
-
-
-
-hikers = []
-hikers.append(Hiker(directives['Gian'],'Gian'))
-hikers.append(Hiker(directives['Gian2'],'Gian2'))
-hikers.append(Hiker(directives['Gian3'],'Gian3'))
-hikers.append(Hiker(directives['Gian4'],'Gian4'))
-
-while not c.is_over():
-
-    print(hikers[2].actual_pos())
-    c.next_iteration('Los cracks', {h.nombre: h.ordenes for h in hikers})
-    if hikers[0].almost_out() is True:
-        hikers[0].random()
-    if hikers[1].almost_out() is True:
-        hikers[1].random()
-    if hikers[2].almost_out() is True:
-        hikers[2].random()
-    if hikers[3].almost_out() is True:
-        hikers[3].random()
-
-
- 
-    time.sleep(0.2) # Para que no colapse el server
+def main():
     
+    team = 'Los cracks'
+    hikers_names = [Hiker('Gian').nombre,Hiker('Pipe').nombre,Hiker('Santi').nombre,Hiker('Joaco').nombre]
+    hikers = [Hiker('Gian'),Hiker('Pipe'),Hiker('Santi'),Hiker('Joaco')]
+    
+    c.add_team(team, hikers_names)
+    c.finish_registration()
 
+    initial_directives = {hikers[0]:{'direction':0,'speed':50}, hikers[1]:{'direction':10,'speed':50}, hikers[2]:{'direction':5,'speed':50}, hikers[3]:{'direction':3,'speed':50}}
+
+    for hiker in hikers:
+        hiker.ordenes = initial_directives[hiker]
+
+    while not c.is_over():
+
+        print(hikers[2].actual_pos())
+        c.next_iteration('Los cracks', {h.nombre: h.ordenes for h in hikers})
+        for hiker in hikers:
+            if hiker.almost_out():
+                hiker.random()
+    
+if __name__ == "__main__":
+    main()
 
