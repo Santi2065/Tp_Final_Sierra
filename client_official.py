3d0c775f786aab92848f74f068bc0fbf960d68d1
Joaco <joacocadeiras@gmail.com>
2023-06-23 16:32:17 -0300

    sdgd

--- client_official.py
+++ client_official.py
@@ -1,6 +1,7 @@
 from communication.client.client import MountainClient
 from HIKERS import Hiker
 from teams import Team
+import time
 from estrategias.empinado import empinado as estrategia
 
 def main():
@@ -12,11 +13,18 @@ def main():
     hikers = [Hiker(name, team_name, c) for name in jugadores]
     team = Team(team_name, hikers, c)
     
+
+    print('Registrando equipo...', end='\r')
+
     c.add_team(team.nombre, jugadores)
     c.finish_registration()
+
+    print('Esperando a comenzar...   ', end='\r')
     while c.is_registering_teams():
+        time.sleep(0.01)
         continue
 
+    print('Haciendo estrategia              ')
     estrategia(team)
     while not c.is_over():
         team.move_all()
