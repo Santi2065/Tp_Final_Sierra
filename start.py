3370f5967b087556c9c27863bc2082e7f1f685e2
FelipeViaggio <fviaggio@udesa.edu.ar>
2023-06-23 01:19:41 -0300

    todo cheto

--- start.py
+++ start.py
@@ -7,5 +7,6 @@ files = ['start_server.py', 'client_official2.py','client_official.py', 'dashpru
 for file in files:
     if platform.system() == 'Windows':
         os.system(f'start cmd /k python {file}')
+        time.sleep(0.5)
     else:
         os.system(f'gnome-terminal -- python3 {file}')
