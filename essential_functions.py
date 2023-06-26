29edf8ec8b09614f08501588570fa4b2bb39e1df
GianlucaM <gmusmarra@udesa.edu.ar>
2023-06-26 18:53:56 -0300

    Docstrings de las funciones

--- essential_functions.py
+++ essential_functions.py
@@ -1,30 +1,131 @@
 import math
 
+def difference(vector1: tuple|list, vector2: tuple|list) -> tuple:
+    '''
+    Calcula las coordenadas del vector que va del vector 2 al vector 1. Ignora la coordenada z.
 
+    Argumentos de entrada: 
+        vector1 (tupla|lista): Coordenadas (x,y) del primer vector.
+        vector2 (tupla|lista): Coordenadas (x,y) del segundo vector.
 
-def difference(p1: tuple|list, p2: tuple|list) -> tuple:
-    '''returns the x y coords of the vector going from p2 to p1, ignores z value'''
-    return (p1[0] - p2[0], p1[1] - p2[1])
+    Salida:
+        Tupla: Coordenadas (x,y) del vector desplazamiento.
+    '''
 
-def dot_product(v1: tuple|list, v2: tuple|list) -> float:
-    '''returns the dot product of v1 and v2, ignores z value'''
-    return v1[0] * v2[0] + v1[1] * v2[1]
+    return (vector1[0] - vector2[0], vector1[1] - vector2[1])
 
-def magnitude(v: tuple[float, float]) -> float:
-    '''returns the magnitude of the vector, ignores z value'''
-    return math.sqrt(math.pow(v[0], 2) + math.pow(v[1], 2))
+def dot_product(vector1: tuple|list, vector2: tuple|list) -> float:
+    '''
+    Calcula el producto interno entre dos vectores. Ignora la coordenada z.
 
-def distance_between(p1: tuple[float, float], p2: tuple[float,float]) -> float:
-    '''returns the distance between p1 and p2, ignores z value'''
-    v = difference(p1, p2)
+    Argumentos de entrada:
+        vector1 (tupla|lista): Coordenadas (x,y) del primer vector.
+        vector2 (tupla|lista): Coordenadas (x,y) del segundo vector.
+
+    Salida:
+        Flotante: Resultado del producto interno entre ambos vectores.
+    '''
+
+    return vector1[0] * vector2[0] + vector1[1] * vector2[1]
+
+def magnitude(vector: tuple[float, float]) -> float:
+    '''
+    Calcula la magnitud de un vector. Ignora la coordenada z.
+    
+    Argumentos de entrada:
+        vector (tupla): Coordenadas (x,y) del vector.
+    
+    Salida:
+        Flotante: Magnitud del vector.
+    '''
+
+    return math.sqrt(math.pow(vector[0], 2) + math.pow(vector[1], 2))
+
+def distance_between(vector1: tuple[float, float], vector2: tuple[float,float]) -> float:
+    '''
+    Calcula la distancia entre dos vectores. Ignora la coordenada z.
+    
+    Argumentos de entrada:
+        vector1 (tupla): Coordenadas (x,y) del primer vector.
+        vector2 (tupla): Coordenadas (x,y) del segundo vector.
+
+    Salida:
+        Flotante: Distancia entre el vector 1 y el vector 2.
+    '''
+
+    v = difference(vector1, vector2)
     return magnitude(v)
 
-def direction(hiker_coord: list|tuple[float, float], objective: list|tuple[float, float]) -> float:
-    '''returns the angle in radians (-pi, pi] for hiker to go in direction to objetive'''
-    dx, dy = difference(objective, hiker_coord)
-    return math.atan2(dy, dx)
+def direction(coordenadas_iniciales: list|tuple[float, float], coordenadas_finales: list|tuple[float, float]) -> float:
+    '''
+    Calcula el angulo necesario a seguir para ir de un punto inicial a un punto final.
 
-def leaderboard(diccionario:dict) -> list: # El original anda (Leaderboard.py)
+    Argumentos de entrada:
+        coordenadas_iniciales (tupla|lista): coordenadas (x,y) del punto de partida.
+        coordenadas_finales (tupla|lista): coordenadas (x,y) del destino.
+
+    Salida:
+        Flotante: Angulo en radianes (-pi, pi] necesario para ir del punto de partida al destino.
+    '''
+
+    distancia_x, distancia_y = difference(coordenadas_finales, coordenadas_iniciales)
+    return math.atan2(distancia_y, distancia_x)
+
+
+def altura_maxima(equipo:str, diccionario:dict,lista_max:list) -> float: 
+    """
+    Calcula la altura maxima alcanzada por un equipo. 
+    
+    Argumentos de entrada:
+        equipo (cadena): Nombre del equipo.
+        diccionario (diccionario): Diccionario con los datos de todos los jugadores.
+        lista_max (lista): Almacena las alturas maximas por iteracion del equipo.
+
+    Salida:
+        Flotante: Altura maxima global alcanzada por el equipo.
+    """
+
+    lista_auxiliar = [] # Almacena momentaneamente todas las alturas del equipo.
+
+
+    for i in diccionario[equipo]:
+        lista_auxiliar.append(diccionario[equipo][i]['z'])
+
+    maximo = max(lista_auxiliar) # Busca el valor maximo de la iteracion actual
+    lista_max.append(maximo) # Almacena todos los maximos de cada iteracion.
+
+    
+    return (max(lista_max)) # devuelve el maximo global
+
+def altura_promedio(diccionario:dict,equipo:str) -> float:
+    
+    """
+    Calcula la altura promedio de todos los jugadores de un equipo por iteracion.
+     
+    Argumentos de salida:
+        diccionario (diccionario) : Diccionario con los datos de todos los jugadores.
+        equipo (cadena) : Nombre del equipo.
+
+    Salida:
+        Flotante: Altura promedio local alcanzada por el equipo redondeada a dos decimales.
+    """
+
+    lista_altura = [] # Almacena momeneamente las alturas de todos los integrantes del equipo.
+
+    for x in diccionario[equipo]:
+        lista_altura.append(diccionario[equipo][x]['z'])
+
+    promedio = sum(lista_altura) / len(lista_altura) # Calcula la altura promedio.
+
+    return round(promedio,2) 
+
+
+
+""" 
+
+# NOSE DONDE ESTA EL NUEVO QUE HIZO PIPE
+
+def leaderboard(diccionario:dict) -> list: 
 
     ''' returns a sorted list with the highest player per team '''
 
@@ -42,33 +143,4 @@ def leaderboard(diccionario:dict) -> list: # El original anda (Leaderboard.py)
 
     return (lista_ordenada) # [nombre,z,equipo]
 
-
-
-
-def altura_maxima(equipo:str, diccionario:dict,lista_max:list) -> float: 
-    """ Returns the highest z reached by the inserted team. """
-    lista_aux = [] # va a guardar las alturas por iteracion de cada integrante del equipo
-
-
-    for i in diccionario[equipo]:
-        lista_aux.append(diccionario[equipo][i]['z'])
-
-    maximo = max(lista_aux)
-    lista_max.append(maximo)
-
-    
-    return (max(lista_max)) 
-
-def altura_promedio(diccionario:dict,equipo:str) -> float:
- 
-    """ Retruns the average height of a team (per iteration) """
-
-    lista_altura = [] 
-    for x in diccionario[equipo]:
-        lista_altura.append(diccionario[equipo][x]['z'])
-
-    promedio = sum(lista_altura) / len(lista_altura) # Calcula el promedio
-
-    return round(promedio,2) 
-
-
+"""
