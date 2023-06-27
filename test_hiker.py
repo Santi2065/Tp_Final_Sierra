aa7d7b67b7c12db4a80497a4c333ce74c27323bf
Joaco <joacocadeiras@gmail.com>
2023-06-27 01:24:03 -0300

    af

--- test_hiker.py
+++ test_hiker.py
@@ -189,7 +189,7 @@ class Graficador:
         fig, ax = self.fig3, self.ax3
         fig.set_figwidth(4.05)
         fig.set_figheight(3)
-        fig.subplots_adjust(top=0.1, bottom=-.1, left=1, right=1)
+        fig.subplots_adjust(bottom=0.5, left=1, right=1)
 
         ax.cla()
 
