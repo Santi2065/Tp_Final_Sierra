af567ead0ae6b9cb80e078b20490926fc6a902c6
GianlucaM <gmusmarra@udesa.edu.ar>
2023-06-29 15:00:12 -0300

    saque lo q habia puesto

--- start.py
+++ start.py
@@ -8,7 +8,5 @@ for file in files:
     time.sleep(2)
     if platform.system() == 'Windows':
         os.system(f'start cmd /k python {file}')
-    elif platform.system()== 'Darwin': # Agregue esto, si no anda algo, sacar esto.
-        os.system(f'python3 {file}') # ...
     else:
         os.system(f'gnome-terminal -- python3 {file}')
