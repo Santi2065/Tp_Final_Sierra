09820a5a0c001acd75631d9a9e7533b129a4ebb2
Santiago Groba <santiagogroba@hotmail.com>
2023-06-29 19:23:50 -0300

    ahora sisi

--- client_official.py
+++ client_official.py
@@ -25,7 +25,7 @@ def main():
     print('Registrando equipo...', end='\r')
 
     c.add_team(team.nombre, names)
-    c.finish_registration()
+    #c.finish_registration()
 
     print('Esperando a comenzar...   ', end='\r')
     while c.is_registering_teams():
