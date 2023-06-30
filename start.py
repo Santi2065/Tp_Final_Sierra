15760a54b4948771da82f487c15ae18c60131086
Santiago Groba <santiagogroba@hotmail.com>
2023-06-29 21:01:10 -0300

    start

--- start.py
+++ start.py
@@ -9,14 +9,16 @@ def main():
     args = parser.parse_args()
     if args.ip:
         ip,puerto = args.ip.split(":")   
-        files = ['start_server.py', f'dashprueba.py --ip {ip}:{puerto}', f'client_official.py --ip {ip}:{puerto}', f'client_official2.py --ip {ip}:{puerto}']
+        files = [f'dashprueba.py --ip {ip}:{puerto}', f'client_official.py --ip {ip}:{puerto}', f'client_official2.py --ip {ip}:{puerto}']
     else:
-        files = ['start_server.py', f'dashprueba.py', f'client_official.py', f'client_official2.py']
+        files = [f'dashprueba.py', f'client_official.py', f'client_official2.py']
 
     for file in files:
         time.sleep(1.5)
         if platform.system() == 'Windows':
             os.system(f'start cmd /k python {file}')
+        elif platform.system() == 'Darwin':
+            os.system(f'open -a Terminal {file}')
         else:
             os.system(f'gnome-terminal -- python3 {file}')
 
